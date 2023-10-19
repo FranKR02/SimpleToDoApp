75cfaf99297c5aed279c420df361d3cf5f924e3a
FranKR02 <frankdiego007@hotmail.com>
2023-10-19 10:29:03 -0500

    Estructura inicial

---
+++ src/test/java/com/projects/todoRest/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.todoRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
